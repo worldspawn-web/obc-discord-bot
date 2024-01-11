cb9bc4e9de36e28896428e65c3133437639671e2
Worldspawn <worldspawn.web@gmail.com>
2024-01-11 15:31:48 +0300

    feat: cooldown for utilities

--- src/commands/utility/server.js
+++ src/commands/utility/server.js
@@ -1,12 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+  cooldown: 10,
   data: new SlashCommandBuilder()
     .setName('server')
     .setDescription('Provides information about the server.'),
   async execute(interaction) {
     await interaction.reply(
-      `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`
+      `Название сервера: ${interaction.guild.name} (${interaction.guild.memberCount} пользователей).`
     );
   },
 };
--- src/commands/utility/user.js
+++ src/commands/utility/user.js
@@ -1,12 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+  cooldown: 10,
   data: new SlashCommandBuilder()
     .setName('user')
     .setDescription('Provides information about the user.'),
   async execute(interaction) {
     await interaction.reply(
-      `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
+      `User: ${interaction.user.username}. Date Joined: ${interaction.member.joinedAt}.`
     );
   },
 };
