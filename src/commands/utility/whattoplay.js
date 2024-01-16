5bccfe01e36bc87ebce563514abdb182e22e726d
Worldspawn <worldspawn.web@gmail.com>
2024-01-16 18:51:31 +0300

    feat: new slash command 'whattoplay'

---
+++ src/commands/utility/whattoplay.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { sp, mp } = require('../../data/games.json');
+
+module.exports = {
+  cooldown: 5,
+  data: new SlashCommandBuilder()
+    .setName('whattoplay')
+    .setDescription('Подкину идей для совместных игр!')
+    .addStringOption((option) =>
+      option
+        .setName('category')
+        .setDescription('Соло или кооп?')
+        .setRequired(true)
+        .addChoices(
+          { name: 'Solo', value: 'sp' },
+          { name: 'Coop', value: 'mp' }
+        )
+    ),
+  async execute(interaction) {
+    const category = interaction.options.getString('category');
+
+    // TODO: Refactor this code.
+    let focusCategory;
+    if (category === 'mp') {
+      focusCategory = mp;
+    } else {
+      focusCategory = sp;
+    }
+
+    const gamesAmount = focusCategory.length;
+    const randomizer = Math.floor(Math.random() * gamesAmount);
+
+    const { game, description, link } = focusCategory[randomizer];
+
+    await interaction.reply(
+      `**${game}** \n\`\`\`${description}\`\`\` \n${link}`
+    );
+  },
+};
